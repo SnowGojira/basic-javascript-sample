ce49838be43f9f63c103f5eab0d34a83256ee733
SirSnow <hakuhal.snow@gmail.com>
2019-03-31 22:07:19 +0800

    make a basic bio style

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,7 +2,20 @@
 Create the json object will be used
  */
 var bio = {
+    name : "HAKU HAL",
+    role : "FRONT-END NINJA",
+    contacts : {
+        mobile : "181-1129-1995",
+        email : "hakuhal@assemblemedia.cn",
+        github : "https://github.com/SirSnow",
+        location : "ChengDu"
+    },
+    welcomeMessage : "",
+    skills : ["Html5","Photoshop","Adobe Illustrator","Motion Graphic","Leadership"],
+    biopic : "",
+    display : function () {
 
+    }
 };
 
 var education = {
